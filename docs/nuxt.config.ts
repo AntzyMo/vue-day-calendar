843fd0df6d3ff939e43edb229871dd546b6bca18
AntzyMo <mozbano@163.com>
2024-04-08 22:55:48 +0800

    chore: 删除nuxt-shiki

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@nuxt/image', 'nuxt-shiki', '@nuxt/content'],
+  modules: ['@unocss/nuxt', '@nuxt/image', '@nuxt/content'],
   css: ['@unocss/reset/tailwind.css']
 })
